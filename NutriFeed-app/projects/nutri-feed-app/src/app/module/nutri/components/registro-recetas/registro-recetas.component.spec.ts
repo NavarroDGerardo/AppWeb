3013acb2e3653cf6b16c7aa97aa93c75862527cf
NanMed <46913776+NanMed@users.noreply.github.com>
2020-10-17 22:56:40 -0500

    registro receta

---
+++ NutriFeed-app/projects/nutri-feed-app/src/app/module/nutri/components/registro-recetas/registro-recetas.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegistroRecetasComponent } from './registro-recetas.component';
+
+describe('RegistroRecetasComponent', () => {
+  let component: RegistroRecetasComponent;
+  let fixture: ComponentFixture<RegistroRecetasComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RegistroRecetasComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RegistroRecetasComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- NutriFeed-app/projects/nutri-feed-app/src/app/module/nutri/nutri-routing.module.ts
+++ NutriFeed-app/projects/nutri-feed-app/src/app/module/nutri/nutri-routing.module.ts
@@ -4,12 +4,14 @@ import { HorarioComponent } from './components/horario/horario.component';
 import { PacientesComponent } from './components/pacientes/pacientes.component';
 import { RecetasComponent } from './components/recetas/recetas.component';
 import { NutriologosComponent } from './components/nutriologos/nutriologos.component';
+import { RegistroRecetasComponent } from './components/registro-recetas/registro-recetas.component';
 
 const routes: Routes = [
   {path: '', component:HorarioComponent},
   {path: "paciente", component:PacientesComponent},
   {path: "recetas", component:RecetasComponent},
-  {path: "nutriologos", component:NutriologosComponent}
+  {path: "nutriologos", component:NutriologosComponent},
+  {path: "registro-recetas", component:RegistroRecetasComponent}
 ];
 
 @NgModule({
