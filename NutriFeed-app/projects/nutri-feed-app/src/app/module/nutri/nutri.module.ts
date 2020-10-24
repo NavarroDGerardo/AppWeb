f2c31d564fd73c0f36b273e826e89670f386fe7b
NanMed <46913776+NanMed@users.noreply.github.com>
2020-10-24 17:59:07 -0500

    Nutri sin ver-receta

--- NutriFeed-app/projects/nutri-feed-app/src/app/module/nutri/nutri.module.ts
+++ NutriFeed-app/projects/nutri-feed-app/src/app/module/nutri/nutri.module.ts
@@ -8,11 +8,10 @@ import { RecetasComponent } from './components/recetas/recetas.component';
 import { NutriologosComponent } from './components/nutriologos/nutriologos.component';
 import { RegistroRecetasComponent } from './components/registro-recetas/registro-recetas.component';
 import { RegistroNutriComponent } from './components/registro-nutri/registro-nutri.component';
-import { VerRecetaComponent } from './components/ver-receta/ver-receta.component';
 
 
 @NgModule({
-  declarations: [HorarioComponent, PacientesComponent, RecetasComponent, NutriologosComponent, RegistroRecetasComponent, RegistroNutriComponent, VerRecetaComponent],
+  declarations: [HorarioComponent, PacientesComponent, RecetasComponent, NutriologosComponent, RegistroRecetasComponent, RegistroNutriComponent],
   imports: [
     CommonModule,
     NutriRoutingModule
