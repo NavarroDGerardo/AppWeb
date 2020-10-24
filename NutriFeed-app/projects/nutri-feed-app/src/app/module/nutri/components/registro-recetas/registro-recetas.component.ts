a6af8bc78794031b36461a07dd4747f0b008b6bd
Gerardo <dgnvrrg@gmail.com>
2020-10-24 17:16:09 -0500

    trabajando en services

--- NutriFeed-app/projects/nutri-feed-app/src/app/module/nutri/components/registro-recetas/registro-recetas.component.ts
+++ NutriFeed-app/projects/nutri-feed-app/src/app/module/nutri/components/registro-recetas/registro-recetas.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RECETA } from '../../../../models/Receta';
 @Component({
   selector: 'app-registro-recetas',
   templateUrl: './registro-recetas.component.html',
   styleUrls: ['./registro-recetas.component.scss']
 })
 export class RegistroRecetasComponent implements OnInit {
-
+  recetas = RECETA;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  registrarReceta(){}
 
 }
