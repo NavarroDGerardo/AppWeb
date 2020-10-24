a3aef2a30ca83a0ed1cc5957bc99dd1edda943b3
JimeBB <jimeberbau@outlook.es>
2020-10-24 18:38:39 -0500

    Ver perfil paciente y progreso (nuevo diseño)

---
+++ NutriFeed-app/projects/nutri-feed-app/src/app/module/nutri/components/ver-perfil-paciente/ver-perfil-paciente.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VerPerfilPacienteComponent } from './ver-perfil-paciente.component';
+
+describe('VerPerfilPacienteComponent', () => {
+  let component: VerPerfilPacienteComponent;
+  let fixture: ComponentFixture<VerPerfilPacienteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ VerPerfilPacienteComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(VerPerfilPacienteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
