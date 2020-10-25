6cef37da4b6723e7fca4232f79fedc44fdb392ba
99carlos99 <A01339011@itesm.mx>
2020-10-24 19:08:29 -0500

    dieta routing

--- NutriFeed-app/projects/nutri-feed-app/src/app/module/nutri/nutri-routing.module.ts
+++ NutriFeed-app/projects/nutri-feed-app/src/app/module/nutri/nutri-routing.module.ts
@@ -7,6 +7,7 @@ import { NutriologosComponent } from './components/nutriologos/nutriologos.compo
 import { RegistroRecetasComponent } from './components/registro-recetas/registro-recetas.component';
 import { RegistroNutriComponent } from './components/registro-nutri/registro-nutri.component';
 import { VerPerfilPacienteComponent } from './components/ver-perfil-paciente/ver-perfil-paciente.component';
+import { RegistroDietaComponent } from './components/registro-dieta/registro-dieta.component'
 
 const routes: Routes = [
   {path: '', component:HorarioComponent},
@@ -15,7 +16,8 @@ const routes: Routes = [
   {path: "nutriologos", component:NutriologosComponent},
   {path: "registro-recetas", component:RegistroRecetasComponent},
   {path: "registro-nutri", component:RegistroNutriComponent},
-  {path: "ver-perfil-paciente", component:VerPerfilPacienteComponent}
+  {path: "ver-perfil-paciente", component:VerPerfilPacienteComponent},
+  {path: "registro-dieta", component:RegistroDietaComponent}
 ];
 
 @NgModule({
