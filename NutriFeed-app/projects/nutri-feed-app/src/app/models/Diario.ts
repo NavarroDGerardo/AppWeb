a90314066129c7717815675e9c8faaf0cde12551
NanMed <46913776+NanMed@users.noreply.github.com>
2020-10-24 22:21:39 -0500

    Correccion forms diario

--- NutriFeed-app/projects/nutri-feed-app/src/app/models/Diario.ts
+++ NutriFeed-app/projects/nutri-feed-app/src/app/models/Diario.ts
@@ -8,11 +8,17 @@ export const DIARIO = [
   {
       desayuno: "Hot cakes de avena y platano",
       comida: "Carne asada",
-      cena: "Pollo asado con verduras"
+      cena: "Pollo asado con verduras",
+      imgDesayuno: "../../../../../assets/img/AvenaZanahoria.png",
+      imgComida: "../../../../../assets/img/TortillaEspinaca.png",
+      imgCena: "../../../../../assets/img/AvenaLeche.png"
   },
   {
     desayuno: "Hot cakes de avena y fresa",
     comida: "Pescado",
-    cena: "Pollo asado con verduras"
+    cena: "Pollo asado con verduras",
+    imgDesayuno: "../../../../../assets/img/AvenaZanahoria.png",
+    imgComida: "../../../../../assets/img/TortillaEspinaca.png",
+    imgCena: "../../../../../assets/img/AvenaLeche.png"
   }
 ]
