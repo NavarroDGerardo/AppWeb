7d32736bc29304487d5b2602b99dfc6ff9ea48c4
NanMed <46913776+NanMed@users.noreply.github.com>
2020-10-24 19:55:03 -0500

    Forms paciente y registro dieta

--- NutriFeed-app/projects/nutri-feed-app/src/app/module/nutri/components/registro-dieta/registro-dieta.component.ts
+++ NutriFeed-app/projects/nutri-feed-app/src/app/module/nutri/components/registro-dieta/registro-dieta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators, FormArray} from '@angular/forms'
 
 @Component({
   selector: 'app-registro-dieta',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroDietaComponent implements OnInit {
 
-  constructor() { }
+  modeloDieta = this.formBuild.group (
+    {
+      desayuno: ['', Validators.required],
+      comida: ['', Validators.required],
+      cena: ['', Validators.required],
+      colaciones: this.formBuild.group({
+        colacion1: ['', Validators.required],
+        colacion2: ['', Validators.required],
+        colacion3: ['', Validators.required]
+      })
+    }
+  );
+
+  constructor(private formBuild:FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  registrarDieta(){
+    console.log(this.modeloDieta.value)
   }
 
 }
