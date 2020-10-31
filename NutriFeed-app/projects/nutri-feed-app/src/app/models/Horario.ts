edc836b2acc1edc00af79c150aff36be5a0537d6
NanMed <46913776+NanMed@users.noreply.github.com>
2020-10-31 02:01:26 -0600

    Servicios

---
+++ NutriFeed-app/projects/nutri-feed-app/src/app/models/Horario.ts
@@ -0,0 +1,38 @@
+export interface Horario{
+  nombre: string;
+  apellido: string;
+  horario: string;
+}
+
+export const HORARIO = [
+  {
+    nombre: "Ana",
+    apellido: "Gómez",
+    horario: "12:00"
+  },
+  {
+    nombre: "Sofia",
+    apellido: "Herrera",
+    horario: "13:00"
+  },
+  {
+    nombre: "Mario",
+    apellido: "Quintero",
+    horario: "14:00"
+  },
+  {
+    nombre: "Elena",
+    apellido: "Nuéz",
+    horario: "15:00"
+  },
+  {
+    nombre: "Victor",
+    apellido: "Ruiz",
+    horario: "17:00"
+  },
+  {
+    nombre: "Antonio",
+    apellido: "Fragoso",
+    horario: "18:00"
+  }
+]
