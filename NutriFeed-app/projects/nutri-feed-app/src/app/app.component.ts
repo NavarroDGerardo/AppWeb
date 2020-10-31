fa817e49c9a9e871dae05045eca6eba00397de16
Gerardo <dgnvrrg@gmail.com>
2020-10-30 23:06:22 -0600

    correcion de animaciones y remover header repetido

--- NutriFeed-app/projects/nutri-feed-app/src/app/app.component.ts
+++ NutriFeed-app/projects/nutri-feed-app/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { animacionesAplicacion } from './Animaciones';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [animacionesAplicacion]
 })
 export class AppComponent {
   title = 'nutriFeed-app';
   isLanding= false;
-  isNutri = true;
-  isPaciente = false;  
+  isNutri = false;
+  isPaciente = true;
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
+  } 
 }
