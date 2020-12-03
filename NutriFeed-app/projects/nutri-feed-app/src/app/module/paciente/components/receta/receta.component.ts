938c174768c56a64d133d5a39aff5ec4692b5392
NanMed <46913776+NanMed@users.noreply.github.com>
2020-12-03 15:19:12 -0600

    Filtro de recetas como paciente

--- NutriFeed-app/projects/nutri-feed-app/src/app/module/paciente/components/receta/receta.component.ts
+++ NutriFeed-app/projects/nutri-feed-app/src/app/module/paciente/components/receta/receta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { Receta } from '../../../../models/Receta';
@@ -12,16 +13,56 @@ import { RecetaService } from '../../../service/receta.service';
 export class RecetaComponent implements OnInit {
   recetas: Receta[] = [];
 
+  modeloBuscar = this.formbuild.group({
+    buscar: ['', Validators.required],
+  });
+
   destroy$: Subject<boolean> = new Subject<boolean>();
 
-  constructor(private recetaService: RecetaService) {}
+  constructor(
+    private recetaService: RecetaService,
+    private formbuild: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.getAllRecetas();
+  }
+
+  getAllRecetas(){
     this.recetaService
       .getRecetas()
       .pipe(takeUntil(this.destroy$))
       .subscribe((data: any[]) => {
         this.recetas = data;
       });
+  }
+
+  buscar(){
+    // console.log('ingrediente', this.modeloBuscar.value);
+    if (this.modeloBuscar.value.buscar == '') {
+      this.getAllRecetas();
+    } else {
+      this.recetaService
+        .buscarRecetaIngrediente(this.modeloBuscar.value.buscar)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe((data: any[]) => {
+          // console.log('la data', data);
+          this.recetas = data;
+        });
+    }
+
+    // this.recetaService
+    //   .buscarRecetaNombre(this.modeloBuscar.value.buscar)
+    //   .pipe(takeUntil(this.destroy$))
+    //   .subscribe((data: any[]) => {
+    //     this.recetas = data;
+    //   });
+
+    // this.recetaService
+    //   .buscarRecetaHashtag(this.modeloBuscar.value.buscar)
+    //   .pipe(takeUntil(this.destroy$))
+    //   .subscribe((data: any[]) => {
+    //     this.recetas = data;
+    //   });
   }
 }
