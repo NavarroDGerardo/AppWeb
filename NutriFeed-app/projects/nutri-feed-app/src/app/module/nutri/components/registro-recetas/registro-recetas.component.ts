d924faa63a5d16a6610502cc072b8013e6c74db8
NanMed <46913776+NanMed@users.noreply.github.com>
2020-12-05 10:08:32 -0600

    Validaciones registrar receta

--- NutriFeed-app/projects/nutri-feed-app/src/app/module/nutri/components/registro-recetas/registro-recetas.component.ts
+++ NutriFeed-app/projects/nutri-feed-app/src/app/module/nutri/components/registro-recetas/registro-recetas.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormArray } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { RecetaService } from '../../../service/receta.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registro-recetas',
@@ -23,7 +24,8 @@ export class RegistroRecetasComponent implements OnInit {
 
   constructor(
     private formbuild: FormBuilder,
-    private recetaService: RecetaService
+    private recetaService: RecetaService,
+    private toastr: ToastrService
   ) {}
 
   ngOnInit(): void {}
@@ -41,20 +43,40 @@ export class RegistroRecetasComponent implements OnInit {
     return `${day}/${month}/${year}`;
   }
 
-  registrarReceta() {
-    const fechaS = new Date();
-    this.modeloReceta.value.fecha = this.formatDate(fechaS);
-
-    const fd = new FormData();
-    fd.append('file', this.selectedFile);
-    fd.append('nombre', this.modeloReceta.value.nombre);
-    fd.append('descripcion', this.modeloReceta.value.descripcion);
-    fd.append('tipo', this.modeloReceta.value.tipo);
-    fd.append('ingrediente', this.modeloReceta.value.ingrediente);
-    fd.append('hashtags', this.modeloReceta.value.hashtags);
-    fd.append('fecha', this.modeloReceta.value.fecha);
-    this.recetaService.insertarReceta(fd);
-    this.modeloReceta.reset();
+  showToastExito(){
+    this.toastr.success('la receta', 'Se registró con éxito');
   }
 
+  showToastError(texto1:string, texto2:string){
+    this.toastr.error(texto2, texto1);
+  }
+
+  registrarReceta() {
+    if (this.modeloReceta.value.nombre == '') {
+      this.showToastError('Nombre incorrecto', 'no puede ser vacío');
+    } else if (this.modeloReceta.value.descripcion ==  '') {
+      this.showToastError('Descripción incorrecta', 'no puede ser vacía');
+    } else if (this.modeloReceta.value.tipo ==  '') {
+      this.showToastError('Tipo incorrecta', 'no puede ser vacío');
+    } else if (this.modeloReceta.value.ingrediente ==  '') {
+      this.showToastError('Ingrediente incorrecto', 'no puede ser vacío');
+    } else if (this.modeloReceta.value.hashtags ==  '') {
+      this.showToastError('Hashtags incorrectos', 'no pueden ser vacíos');
+    } else {
+      const fechaS = new Date();
+      this.modeloReceta.value.fecha = this.formatDate(fechaS);
+
+      const fd = new FormData();
+      fd.append('file', this.selectedFile);
+      fd.append('nombre', this.modeloReceta.value.nombre);
+      fd.append('descripcion', this.modeloReceta.value.descripcion);
+      fd.append('tipo', this.modeloReceta.value.tipo);
+      fd.append('ingrediente', this.modeloReceta.value.ingrediente);
+      fd.append('hashtags', this.modeloReceta.value.hashtags);
+      fd.append('fecha', this.modeloReceta.value.fecha);
+      this.recetaService.insertarReceta(fd);
+      this.modeloReceta.reset();
+      this.showToastExito();
+    }
+  }
 }
